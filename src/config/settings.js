33ad675338b875ba1528705c4387728a362c96ab
Arjun Choudhary <contact@arjunchoudhary.com>
2023-06-17 15:00:49 +0530

    Updates as required

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Rajni Chemical`,
+	description: `Rajni Chemical Industries PVT LTD`,
+	url: `/rajnichem`, // No trailing slash!
+	name: `Rajni Chemical`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
